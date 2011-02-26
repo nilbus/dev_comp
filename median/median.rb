24c0be932ecc2feb4d9ec04cc16b311c927956e6
Ben Berry <bsberry@ncsu.edu>
2011-02-26 12:07:14 -0500

    Submission 2

--- median/median.rb
+++ median/median.rb
@@ -2,12 +2,8 @@ def median(num_list)
   num_list.sort!
   
   #if the list is of an odd-numbered size
-  if(num_list.size % 1)
-    index = num_list.size / 2 + 1
-    return num_list[index-1]
-  else
-    return (num_list[num_list.size - 1] + num_list[num_list.size]) / 2
-  end
+  index = num_list.size / 2 + 1
+  return num_list[index-1]
 
 end
 
@@ -17,6 +13,8 @@ num_sets = $stdin.gets.strip.to_i
   info_line = $stdin.gets.strip.split
   info_line = info_line.map{ |x| x.to_i}
 
+  num_nums = info_line[1]
+
   info_line = $stdin.gets.strip.split
   nums = info_line.map{ |x| x.to_i}
 
@@ -25,8 +23,10 @@ num_sets = $stdin.gets.strip.to_i
 
   print_flag = true
 
-  nums.each do |num|
+  0.upto num_nums do |index|
+    num = nums[index]
     l.push(num)
+
     if(print_flag)
       medians.push(median(l))
       print_flag = false
