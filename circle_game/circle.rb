7763e12b53eb18f6ed37897a266966ecf8e7ea97
Edward Anderson <nilbus@nilbus.com>
2011-02-26 11:31:45 -0500

    completed #1

--- circle_game/circle.rb
+++ circle_game/circle.rb
@@ -3,20 +3,44 @@
 require './lib/linked_list'
 
 def main
-  input = STDIN.each_line
-  input.to_a.shift # Discard first line
+  log 'main'
+  input = STDIN.read.each_line.to_a
+  input.shift # Discard first line
   input.each do |line|
-    n,k = line.split.map &:to_i
-    raise 'expected n to be integer' unless n.is_a? Fixnum
-    raise 'expected k to be integer' unless k.is_a? Fixnum
+    n,k = line.split.map{|i|i.strip.to_i}
+    log "n: #{n.inspect} k: #{k.inspect}"
+    raise "expected n to be integer #{n.inspect}" unless n.is_a? Fixnum
+    raise "expected k to be integer #{k.inspect}" unless k.is_a? Fixnum
     play_game(n, k)
   end
 end
 
 def play_game(n, k)
+  # build circle
   circle = LinkedList.new
   (1..n).each {|i| circle.addLast(i.to_s)}
-  circle
+
+  # remove people until winner is found
+  current = circle.first_node
+  (1..n-1).each do |i|
+    log "round #{i}"
+    (1..k).each do |j|
+      current = current.next
+      current = current.next if current.object.nil? # there's one node that is the null node
+    end
+    # remove and print current node
+    current.prev.next = current.next
+    current.next.prev = current.prev
+    log "removing #{current.object}"
+    current = current.next
+    current = current.next if current.object.nil? # there's one node that is the null node
+  end
+
+  # print the winner
+  puts circle.first
 end
 
+def log(msg)
+  STDERR.write("#{msg}\n")
+end
 main
